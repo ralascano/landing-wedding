19009bbcc89795d8b6322a7df8380c47efc616b2
ralascanog <Ricardo.Gonzalez@vela1.com>
2025-04-30 15:47:45 -0500

    fix: changing env variables name

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getStorage } from "firebase/storage";
 import { getAuth, signInAnonymously } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.APIKEY,
-  authDomain: import.meta.env.AUTHDOMAIN,
-  projectId: import.meta.env.PROJECTID,
-  storageBucket: import.meta.env.STORAGEBUCKET,
-  messagingSenderId: import.meta.env.MESSAGINGSENDERID,
-  appId: import.meta.env.APPID,
-  measurementId: import.meta.env.MEASUREMENTID
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASUREMENTID
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
